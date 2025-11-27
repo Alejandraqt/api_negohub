844a4cbf284f7247ebf4bd945b9576a843bbd355
Sharon Cruz <sharina.1401@gmail.com>
2025-11-27 18:18:48 -0500

    puerto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const parser = require("body-parser");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const negocioRoutes = require("./routes/negocioRoutes");
 const usuarioRoutes = require("./routes/usuarioRoutes");
